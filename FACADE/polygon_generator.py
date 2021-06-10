c355c62e8064c503d6845bc0826c353ba9195aa0
MechMel <MelchiahMauck@gmail.com>
2021-06-09 20:49:55 -0700

    Ran some tests on artificial MNIST.

--- FACADE/polygon_generator.py
+++ FACADE/polygon_generator.py
@@ -2,7 +2,7 @@ import multiprocessing
 import random
 import numpy as np
 from src.facade import FACADE
-from src.blc import ConnectedSet, Point, Line, BLC, BLC_Utils
+from src.blc import Point, Line, BLC, BLC_Utils
 
 def generateAPerfectTriangle():
   # Create the points and lines
@@ -57,21 +57,20 @@ def generateAPerfectHexagon():
     
   allPointsInShape = None
   # Create the points and lines
-  # if statements not necessary with rotation
-  if random.random() < 0.5: # pointy top
-      pointA = Point(BLC_Utils.randDev(0.50, 0.05), BLC_Utils.randDev(0.20, 0.05)) 
-      pointB = Point(BLC_Utils.randDev(0.70, 0.05), BLC_Utils.randDev(0.30, 0.05)) 
-      pointC = Point(BLC_Utils.randDev(0.70, 0.05), BLC_Utils.randDev(0.70, 0.05)) 
-      pointD = Point(BLC_Utils.randDev(0.50, 0.05), BLC_Utils.randDev(0.80, 0.05)) 
-      pointE = Point(BLC_Utils.randDev(0.30, 0.05), BLC_Utils.randDev(0.70, 0.05)) 
-      pointF = Point(BLC_Utils.randDev(0.30, 0.05), BLC_Utils.randDev(0.30, 0.05)) 
-  else: # wide side at the top
-      pointA = Point(BLC_Utils.randDev(0.30, 0.05), BLC_Utils.randDev(0.30, 0.05)) 
-      pointB = Point(BLC_Utils.randDev(0.70, 0.05), BLC_Utils.randDev(0.30, 0.05)) 
-      pointC = Point(BLC_Utils.randDev(0.80, 0.05), BLC_Utils.randDev(0.50, 0.05)) 
-      pointD = Point(BLC_Utils.randDev(0.70, 0.05), BLC_Utils.randDev(0.70, 0.05)) 
-      pointE = Point(BLC_Utils.randDev(0.30, 0.05), BLC_Utils.randDev(0.70, 0.05)) 
-      pointF = Point(BLC_Utils.randDev(0.20, 0.05), BLC_Utils.randDev(0.50, 0.05)) 
+  if random.random() < 0.1: # narrow shape
+      pointA = Point(BLC_Utils.randRange(0.45, 0.55), BLC_Utils.randRange(0.15, 0.20)) # (0.50, 0.15)
+      pointB = Point(BLC_Utils.randRange(0.65, 0.75), BLC_Utils.randRange(0.30, 0.35)) # (0.70, 0.30)
+      pointC = Point(BLC_Utils.randRange(0.65, 0.75), BLC_Utils.randRange(0.65, 0.70)) # (0.70, 0.70)
+      pointD = Point(BLC_Utils.randRange(0.45, 0.55), BLC_Utils.randRange(0.80, 0.85)) # (0.50, 0.85)
+      pointE = Point(BLC_Utils.randRange(0.25, 0.35), BLC_Utils.randRange(0.65, 0.70)) # (0.30, 0.70)
+      pointF = Point(BLC_Utils.randRange(0.25, 0.35), BLC_Utils.randRange(0.30, 0.35)) # (0.30, 0.30)
+  else:
+      pointA = Point(BLC_Utils.randRange(0.30, 0.35), BLC_Utils.randRange(0.25, 0.30)) # (0.30, 0.30)
+      pointB = Point(BLC_Utils.randRange(0.65, 0.70), BLC_Utils.randRange(0.25, 0.30)) # (0.70, 0.30)
+      pointC = Point(BLC_Utils.randRange(0.80, 0.85), BLC_Utils.randRange(0.45, 0.55)) # (0.85, 0.50)
+      pointD = Point(BLC_Utils.randRange(0.65, 0.70), BLC_Utils.randRange(0.65, 0.70)) # (0.70, 0.70)
+      pointE = Point(BLC_Utils.randRange(0.30, 0.35), BLC_Utils.randRange(0.65, 0.70)) # (0.30, 0.70)
+      pointF = Point(BLC_Utils.randRange(0.15, 0.20), BLC_Utils.randRange(0.45, 0.55)) # (0.15, 0.50)
   lineAB = Line(pointA, pointB)
   lineBC = Line(pointB, pointC)
   lineCD = Line(pointC, pointD)
@@ -85,80 +84,21 @@ def generateAPerfectHexagon():
   # Return the BLC
   return BLC.traversePointsToCreateBLC(allPointsInShape)
   
-def generateAPerfectOctagon():
-    
-  allPointsInShape = None
-  
-  pointA = Point(BLC_Utils.randDev(0.15, 0.01), BLC_Utils.randDev(0.35, 0.01)) 
-  pointB = Point(BLC_Utils.randDev(0.35, 0.01), BLC_Utils.randDev(0.15, 0.01)) 
-  pointC = Point(BLC_Utils.randDev(0.65, 0.01), BLC_Utils.randDev(0.15, 0.01)) 
-  pointD = Point(BLC_Utils.randDev(0.85, 0.01), BLC_Utils.randDev(0.35, 0.01)) 
-  pointE = Point(BLC_Utils.randDev(0.85, 0.01), BLC_Utils.randDev(0.65, 0.01)) 
-  pointF = Point(BLC_Utils.randDev(0.65, 0.01), BLC_Utils.randDev(0.85, 0.01)) 
-  pointG = Point(BLC_Utils.randDev(0.35, 0.01), BLC_Utils.randDev(0.85, 0.01)) 
-  pointH = Point(BLC_Utils.randDev(0.15, 0.01), BLC_Utils.randDev(0.65, 0.01)) 
-  
-  lineAB = Line(pointA, pointB)
-  lineBC = Line(pointB, pointC)
-  lineCD = Line(pointC, pointD)
-  lineDE = Line(pointD, pointE)
-  lineEF = Line(pointE, pointF)
-  lineFG = Line(pointF, pointG)
-  lineGH = Line(pointG, pointH)
-  lineHA = Line(pointH, pointA)
-  
-  allPointsInShape = [pointA, pointB, pointC, pointD, pointE, pointF, pointG, pointH]
-  BLC_Utils.rotatePointsAroundOrigin(allPointsInShape, BLC_Utils.randRange(-180.0, 180.0))
-  
-  # Return the BLC
-  return BLC.traversePointsToCreateBLC(allPointsInShape)  
-  
-def generateAPerfectCircle(): # similar to zero MNIST shape
-    allPointsInCircle = []
-
-    # Draw the circle
-    topLeft = Point(BLC_Utils.randDev(0.45, 0.045), BLC_Utils.randDev(0.225, 0.05))
-    bottom = Point(BLC_Utils.randDev(0.5, 0.035), BLC_Utils.randDev(0.775, 0.05))
-    topRight = Point(BLC_Utils.randDev(0.55, 0.045), BLC_Utils.randDev(0.225, 0.05))
-    Line(topLeft, bottom, peakMagnitude=BLC_Utils.randRange(-1.0, -0.4), peakOffset=BLC_Utils.randRange(-0.25, 0.25))
-    Line(bottom, topRight, peakMagnitude=BLC_Utils.randRange(-1.0, -0.4), peakOffset=BLC_Utils.randRange(-0.25, 0.25))
-    allPointsInCircle.extend([topLeft, bottom, topRight])
-
-    # Add some random rotation
-    BLC_Utils.rotatePointsAroundOrigin(allPointsInCircle, BLC_Utils.randRange(-180.0, 180.0))
-
-    # Create a BLC for the circle
-    circleBLC = BLC.traversePointsToCreateBLC(allPointsInCircle)
-    
-    # Sometimes add a slash
-    if random.random() < 0.2:
-        slashTop = Point(BLC_Utils.randDev(0.7, 0.05), BLC_Utils.randDev(0.25, 0.035))
-        slashBottom = Point(BLC_Utils.randDev(0.3, 0.05), BLC_Utils.randDev(0.75, 0.035))
-        if random.random() < 0.5:
-          BLC_Utils.rotatePointsAroundOrigin([slashTop, slashBottom], 120.0)
-        slash = Line(slashTop, slashBottom)
-        startPoint = Point(slashTop.x, slashTop.y)
-        peakPoint = slash.getPeakAsPoint()
-        endPoint = Point(slashBottom.x, slashBottom.y)
-        slashSet = ConnectedSet([startPoint, peakPoint, endPoint])
-        circleBLC.connectedSets.append(slashSet)
-    
-    return circleBLC
-
-  
 # Setup for using multiple processors
 if __name__ == '__main__':
   multiprocessing.freeze_support()
-  
-  # Define the classes
-  facade = FACADE([
-    FACADE.ClassPropertiesObject('triangle', generateAPerfectTriangle),
-    FACADE.ClassPropertiesObject('square', generateAPerfectSquare),
-    FACADE.ClassPropertiesObject('rectangle', generateAPerfectRectangle),
-    FACADE.ClassPropertiesObject('hexagon', generateAPerfectHexagon),
-    FACADE.ClassPropertiesObject('octagon', generateAPerfectOctagon),
-    FACADE.ClassPropertiesObject('circle', generateAPerfectCircle)
-  ])
-  
-  # Now generate the dataset
+
+  # This associates the above function with the class name 'triangle'
+  triangleClassProperties = FACADE.ClassPropertiesObject('triangle', generateAPerfectTriangle)
+  # This associates the above function with the class name 'square'
+  squareClassProperties = FACADE.ClassPropertiesObject('square', generateAPerfectSquare)
+  # This associates the above function with the class name 'rectangle'
+  rectangleClassProperties = FACADE.ClassPropertiesObject('rectangle', generateAPerfectRectangle)
+  # This associates the above function with the class name 'rectangle'
+  hexagonClassProperties = FACADE.ClassPropertiesObject('hexagon', generateAPerfectHexagon)
+
+
+  # We then create a new FACADE instance with our set of classes
+  facade = FACADE([triangleClassProperties, squareClassProperties, rectangleClassProperties, hexagonClassProperties])
+  # And ask it to output several images from each of the classes we gave it
   facade.generateDataset(100, 'outputs/polygons')
---
+++ Huy/src/utils.py
@@ -0,0 +1,213 @@
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import random as random
+import math as math
+import copy
+from PIL import Image
+from os import listdir, mkdir
+from os.path import isdir, isfile, join
+
+
+def emptyFile(filePath):
+  fileToEmpty = open(filePath, 'w')
+  fileToEmpty.write('')
+  fileToEmpty.close()
+
+
+def getPrecentText(subCount, totalCount):
+  return "{:.2f}%".format(100 * (subCount / totalCount))
+
+
+tempOutputFile = None
+def saveModelSummary(model, outputFilePath):
+  def writeLineToOutputFile(line):
+    tempOutputFile.write(line + '\n')
+  emptyFile(outputFilePath)
+  tempOutputFile = open(outputFilePath, 'a')
+  model.summary(print_fn=writeLineToOutputFile)
+  tempOutputFile.close()
+
+
+def getNoisyVariants(originalImages, directoryPath):
+  noisyImages = []
+  # Either load from a file, or create new ones
+  if (isdir(directoryPath) and isfile(join(directoryPath, 'image_0.jpg'))):
+    print('\nUsing saved noisy images from: ' + directoryPath)
+    fileNames = [fileName for fileName in listdir(directoryPath) if isfile(join(directoryPath, fileName))]
+    for fileName in fileNames:
+      filePath = directoryPath + '/' + fileName
+      image = img_to_array(load_img(filePath, color_mode="grayscale"))
+      noisyImages.append(image)
+  else: # If no save images were found, then generate new ones
+    #mkdir(directoryPath)
+    print('\nAdding noise and saving to: ' + directoryPath)
+    imageCount = len(originalImages)
+    imageIndex = 0
+    # Copy the images so we don't overwrite anything
+    copiesOfOriginalImages = []
+    for originalImage in originalImages:
+      copiesOfOriginalImages.append(copy.deepcopy(originalImage))
+    # Add gaussian noise to each image
+    for originalImage in copiesOfOriginalImages:
+      image = originalImage.reshape((28, 28))
+      gaussianMask = np.random.normal(0.0, 1.0, (28, 28))
+      for x in range(28):
+        for y in range(28):
+          image[x, y] = np.average([image[x, y], gaussianMask[x][y]], weights=[1, 1])
+      image = image.reshape((28, 28, 1))
+      array_to_img(image).save(directoryPath + '/image_' + str(imageIndex) + '.jpg')
+      noisyImages.append(image)
+      imageIndex += 1
+      if (imageIndex % math.floor(imageCount / 20) == 0):
+        print('Completed ' + str(imageIndex) + ' of ' + str(imageCount) + ' images.')
+  return np.array(noisyImages)
+
+
+def plotTrainAndTestLoss(trainLoss, testLoss, filePath):
+  plt.figure()
+  plt.xlabel('epochs')
+  plt.ylabel('loss')
+  plt.ylim([0, 3])
+  plt.plot(trainLoss, color='#FF0000', label='Trainning Data')
+  plt.plot(testLoss, color='#0000FF', label='Test Data')
+  plt.legend()
+  plt.savefig(filePath)
+
+
+def plotTrainAndTestAccuracy(trainAccuracy, testAccuracy, filePath):
+  plt.figure()
+  plt.xlabel('epochs')
+  plt.ylabel('accuracy')
+  plt.ylim([0.0, 1.0])
+  plt.plot(trainAccuracy, color='#FF0000', label='Trainning Data')
+  plt.plot(testAccuracy, color='#0000FF', label='Test Data')
+  plt.legend()
+  plt.savefig(filePath)
+
+
+def printSomeDatasetSamples(images, labels, filePath):
+  fig, subplots = plt.subplots(5, 5)
+
+  for x in range(5):
+    for y in range(5):
+      imageIndex = random.randint(0, len(images) - 1)
+      classIndex = labels[imageIndex].tolist().index(1.0)
+      subplots[x, y].axis('off')
+      subplots[x, y].title.set_text(str(classIndex))
+      subplots[x, y].imshow(images[imageIndex], cmap=plt.get_cmap('gray'))
+      
+  plt.savefig(filePath)
+  plt.close()
+
+
+def printSomeAutoencoderSamples(autoencoder, testImages, expectedImages=None, filePath='output/autoencoder_samples.jpg'):
+  # Figure out the plot configuration
+  subplotRowCount = 2
+  if (expectedImages is not None):
+    subplotRowCount = 3
+  fig, subplots = plt.subplots(subplotRowCount, 4)
+
+  # Print a few images
+  for sampleIndex in range(4):
+    # Get the input image
+    imageIndex = random.randint(0, len(testImages) - 1)
+    rawInputImage = testImages[imageIndex]
+    printableInputImage = rawInputImage.reshape((28, 28))
+
+    # Get the output image
+    rawOutputImage = autoencoder.predict(np.array([rawInputImage]))[0]
+    printableOutputImage = rawOutputImage.reshape((28, 28))
+
+    # Add the input and output images to the figure
+    subplots[0, sampleIndex].axis('off')
+    subplots[0, sampleIndex].title.set_text('Input ' + str(sampleIndex + 1))
+    subplots[0, sampleIndex].imshow(printableInputImage, cmap=plt.get_cmap('gray'))
+    subplots[1, sampleIndex].axis('off')
+    subplots[1, sampleIndex].title.set_text('Output ' + str(sampleIndex + 1))
+    subplots[1, sampleIndex].imshow(printableOutputImage, cmap=plt.get_cmap('gray'))
+    if (expectedImages is not None):
+      printableExpectedImage = expectedImages[imageIndex].reshape((28, 28))
+      subplots[2, sampleIndex].axis('off')
+      subplots[2, sampleIndex].title.set_text('Expected ' + str(sampleIndex + 1))
+      subplots[2, sampleIndex].imshow(printableExpectedImage, cmap=plt.get_cmap('gray'))
+  plt.savefig(filePath)
+
+
+def plotABunchOfEncoderSamplesForEachFashionClass(encode, testImages, testClassLabels, filePath):
+  plt.figure()
+  colorsByClass = getADiverseSetOfColor(10)
+  maxX = 0.1
+  maxY = 0.1
+
+  # Sort the test images by class
+  testImagesByClass = [[] for _ in range(10)]
+  for testImageIndex in range(len(testImages)):
+    testImage = testImages[testImageIndex]
+    classIndex = testClassLabels[testImageIndex]
+    testImagesByClass[classIndex].append(testImage)
+
+  # Pick a few hundered random images from each class
+  imagesToPlotByClass = [[] for _ in range(10)]
+  for classIndex in range(10):
+    imagesInThisClass = testImagesByClass[classIndex]
+    numberOfImagesInThisClass = len(imagesInThisClass)
+    for _ in range(250):
+      imageIndex = random.randint(0, numberOfImagesInThisClass - 1)
+      imagesToPlotByClass[classIndex].append(imagesInThisClass[imageIndex])
+
+  # Encode the images
+  pointsByClass = encode(np.array(imagesToPlotByClass))
+  
+  # Plot a bunch of samples for each class
+  for classIndex in range(10):
+    points = pointsByClass[classIndex]
+    pointsX = [point[0] for point in points]
+    pointsY = [point[1] for point in points]
+    maxX = np.max([maxX, np.max(pointsX)])
+    maxY = np.max([maxY, np.max(pointsY)])
+    plt.scatter(pointsX, pointsY, color=colorsByClass[classIndex], s=3)
+
+  # Add some extra configuration to the plot
+  plt.xlim([0, 0.1 + (maxX * 1.5)]) # Leave some space for the legend
+  plt.ylim([0, 0.1 + (maxY * 1.1)])
+  print('max x: ' + str(maxX) + ', max y: ' + str(maxY))
+  plt.legend(
+    handles=[
+      mpatches.Patch(color=colorsByClass[0], label='T-shirt/top'),
+      mpatches.Patch(color=colorsByClass[1], label='Trouser'),
+      mpatches.Patch(color=colorsByClass[2], label='Pullover'),
+      mpatches.Patch(color=colorsByClass[3], label='Dress'),
+      mpatches.Patch(color=colorsByClass[4], label='Coat'),
+      mpatches.Patch(color=colorsByClass[5], label='Sandal'),
+      mpatches.Patch(color=colorsByClass[6], label='Shirt'),
+      mpatches.Patch(color=colorsByClass[7], label='Sneaker'),
+      mpatches.Patch(color=colorsByClass[8], label='Bag'),
+      mpatches.Patch(color=colorsByClass[9], label='Ankle boot'),
+    ],
+    loc='upper right')
+  plt.savefig(filePath)
+
+
+def getADiverseSetOfColor(colorCount):
+  BRIGHTNESS = 200
+  RED_THRESHOLD = BRIGHTNESS
+  GREEN_THRESHOLD = BRIGHTNESS * 2
+  BLUE_THRESHOLD = BRIGHTNESS * 3
+  COLOR_STEP = (BRIGHTNESS * 3) / colorCount
+
+  # Spread out the colors
+  colors = [None for i in range(colorCount)]
+  for i in range(colorCount):
+    colorAsNumber = math.floor(i * COLOR_STEP)
+    if (0 <= colorAsNumber < RED_THRESHOLD):
+      colorTuple = (RED_THRESHOLD - colorAsNumber, colorAsNumber, 0)
+    elif (RED_THRESHOLD <= colorAsNumber < GREEN_THRESHOLD):
+      colorTuple = (0, GREEN_THRESHOLD - colorAsNumber, colorAsNumber - RED_THRESHOLD)
+    else:
+      colorTuple = (colorAsNumber - GREEN_THRESHOLD, 0, BLUE_THRESHOLD - colorAsNumber)
+    # Convert to hexcode
+    colors[i] = '#%02x%02x%02x' % colorTuple
+
+  return colors
