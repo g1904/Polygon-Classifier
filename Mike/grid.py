2b0ed2749bba51b9d78e8fcd88aad941e9ae944c
Mike <y1904840334@yahoo.com>
2021-06-10 03:06:30 -0700

    NeatSet created.

---
+++ Mike/grid.py
@@ -0,0 +1,39 @@
+import math
+from PIL import Image
+
+# Controls
+inputImageBasePath = 'data/neat-set-'
+outputBasePath = 'NeatSet/neat-set-'
+authorName = ['a', 'b', 'c', 'd', 'e']
+indexToSheetName = ['line', 'triangle', 'rectangle', 'diamond', 'hexagon', 'octagon', 'circle' ]
+cellCountH = 4
+cellCountV = 5
+cellMarginRatio = 0
+sheetImageWidth = 1382
+sheetImageHeight = 1727
+
+for j in range(5):
+    for i in range(7):
+        # Load and stretch the sheet
+        inputImagePath = inputImageBasePath + authorName[j] + '-sheets-' + str(i) + '.jpg'
+        outputPathStub = outputBasePath + indexToSheetName[i] + "-" + authorName[j] + '-'
+        sheet = Image.open(inputImagePath)
+        sheet = sheet.resize((sheetImageWidth, sheetImageHeight))
+        sheetOutputImageName = outputPathStub + 'sheet.jpg'
+
+        # Calcualte values
+        cellWidth = sheetImageWidth / cellCountH
+        cellMargin = cellWidth * cellMarginRatio
+
+        # Punch out the individual cells
+        for x in range(cellCountH):
+            for y in range(cellCountV):
+                left = math.floor(x * cellWidth + cellMargin)
+                top = math.floor(y * cellWidth + cellMargin)
+                right = math.floor((x + 1) * cellWidth - cellMargin)
+                bottom = math.floor((y + 1) * cellWidth - cellMargin)
+
+                cellImageName = outputPathStub + str(x + (y * cellCountV)) + '.jpg'
+                cellImage = sheet.crop((left, top, right, bottom))
+                cellImage = cellImage.convert('L') # Convert to black and white
+                cellImage.save(cellImageName)
